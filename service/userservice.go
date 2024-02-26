48046329e5a9c4de7026d71e9189544853b167f3
18lis <jack0712li@gmail.com>
2024-02-26 01:42:00 -0500

    Create CRUD for User Model

--- service/userservice.go
+++ service/userservice.go
@@ -1,21 +1,93 @@
 package service
 
 import (
+	"fmt"
 	"ginchat/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-// GetIndex
-// @Tags userBasic
+// GetUserList
+// @Summary user list
+// @Tags User
 // @Success 200 {string} json{"code", "message"}
 // @Router /user/getUserList [get]
-func GetUserList(c *gin.Context){
+func GetUserList(c *gin.Context) {
 	data := models.GetUserList()
-
 
 	c.JSON(200, gin.H{
 		"message": data,
 	})
 
+}
+
+// CreateUser
+// @Summary create user
+// @Tags User
+// @param name query string false "userName"
+// @param password query string false "passWord"
+// @param repassword query string false "repassWord"
+// @Success 200 {string} json{"code", "message"}
+// @Router /user/createUser [get]
+func CreateUser(c *gin.Context) {
+	user := models.UserBasic{}
+
+	user.Name = c.Query("name")
+	password := c.Query("password")
+	repassword := c.Query("repassword")
+
+	if password != repassword {
+		c.JSON(-1, gin.H{
+			"message": "password is not same",
+		})
+		return
+	}
+	user.Password = password
+	models.CreateUser(user)
+	c.JSON(200, gin.H{
+		"message": "create user success",
+	})
+
+}
+
+// DeleteUser
+// @Summary delete user
+// @Tags User
+// @param id query string false "id"
+// @Success 200 {string} json{"code", "message"}
+// @Router /user/deleteUser [get]
+func DeleteUser(c *gin.Context) {
+	user := models.UserBasic{}
+
+	id, _ := strconv.Atoi(c.Query("id"))
+	user.ID = uint(id)
+	models.DeleteUser(user)
+	c.JSON(200, gin.H{
+		"message": "Delete user success",
+	})
+}
+
+
+// UpdateUser
+// @Summary update user
+// @Tags User
+// @param id formData string false "id"
+// @param name formData string false "name"
+// @param password formData string false "passWord"
+// @Success 200 {string} json{"code", "message"}
+// @Router /user/updateUser [post]
+func UpdateUser(c *gin.Context) {
+	user := models.UserBasic{}
+
+	id, _ := strconv.Atoi(c.PostForm("id"))
+	user.ID = uint(id)
+	user.Name = c.PostForm("name")
+	user.Password = c.PostForm("password")
+	fmt.Println(user)
+
+	models.UpdateUser(user)
+	c.JSON(200, gin.H{
+		"message": "Update user success",
+	})
 }
